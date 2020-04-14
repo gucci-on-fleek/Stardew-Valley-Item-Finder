fb73eb8579cceeadd35e439f2dcf5f5a5e58fd9f
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2020-04-14 17:05:53 -0600

    Add service worker support for offline usage

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+var cache_name = 'stardew-valley-item-finder-v0.4'
+var cache = caches.open(cache_name)
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(cache.then(c => c.addAll([
+    '/',
+    'index.html',
+    'item-finder.js',
+    'item-finder.css',
+    'items.xslt',
+    'items-to-csv.xslt',
+    'assets/gold_star.png',
+    'assets/silver_star.png',
+    'assets/iridium_star.png',
+    'libraries/tablesort.min.js'
+  ])))
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)))
+})
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(function () {
+    caches.keys()
+      .then(x =>
+        x.filter(y => y !== cache_name)
+          .map(z => caches.delete(z)))
+  })
+})
