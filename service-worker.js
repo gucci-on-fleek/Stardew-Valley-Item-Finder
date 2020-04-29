89072c206f53267a2ac4f5aa4ba9d84a6fe59f90
gucci-on-fleek <gucci-on-fleek@users.noreply.github.com>
2020-04-29 02:09:04 +0000

    deploy: c956a724ba835a797363f936f7679ea39fa8a6a8

--- service-worker.js
+++ service-worker.js
@@ -4,30 +4,39 @@
  * Licensed under MPL 2.0 or greater. See URL for more information.
  */
 
-const version="89813911"
+const version="90741192"
 const cache_name = `stardew-valley-item-finder-v${version}`
 const cache = caches.open(cache_name)
+const requests = [
+  '.',
+  'index.html',
+  'item-finder.min.js',
+  'item-finder.min.css',
+  'items.min.xslt',
+  'items-to-csv.min.xslt',
+  'service-worker.min.js',
+  'assets/gold_star.png',
+  'assets/silver_star.png',
+  'assets/iridium_star.png',
+  'assets/icon.svg',
+  'assets/icon-32.png',
+  'libraries/tablesort.min.js',
+  'manifest.webmanifest'
+]
+let fetched = 0
+const threshold = 1000 * 60 * 15 // 15 minutes in milliseconds
 
 self.addEventListener('install', function (event) {
-  event.waitUntil(cache.then(c => c.addAll([
-    '.',
-    'index.html',
-    'item-finder.min.js',
-    'item-finder.min.css',
-    'items.min.xslt',
-    'items-to-csv.min.xslt',
-    'service-worker.min.js',
-    'assets/gold_star.png',
-    'assets/silver_star.png',
-    'assets/iridium_star.png',
-    'assets/icon.svg',
-    'assets/icon-32.png',
-    'libraries/tablesort.min.js'
-  ])))
+  event.waitUntil(cache.then(c => c.addAll(requests)))
+  fetched = Date.now()
 })
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)))
+  event.waitUntil(refresh_all())
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request)) // Respond with the current cache, or a network request if not present
+  )
 })
 
 self.addEventListener('activate', function (event) {
@@ -35,6 +44,26 @@ self.addEventListener('activate', function (event) {
     caches.keys()
       .then(x =>
         x.filter(y => y !== cache_name)
-          .map(z => caches.delete(z)))
+          .map(z => caches.delete(z))) // Delete old caches
   })
 })
+
+async function refresh(resource) {
+  return cache.then(c => fetch(resource, { cache: 'no-cache' })
+    .then(x => c.put(resource, x)) // Fetch from the network and store in cache
+  )
+}
+
+function refresh_all() {
+  if (Date.now() - fetched > threshold) { // Update cache if more than `threshold` time has passed
+    let promises = []
+    for (let i = 0; i < requests.length; i++) {
+      promises.push(refresh(requests[i]))
+    }
+    fetched = Date.now()
+
+    return Promise.all(promises)
+  } else {
+    return Promise.resolve() // Return an empty promise otherwise
+  }
+}
