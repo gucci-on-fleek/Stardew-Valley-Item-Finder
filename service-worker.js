d2e075ee6f38ada825ef254813ab3a5ebe887e64
gucci-on-fleek <gucci-on-fleek@users.noreply.github.com>
2020-05-16 07:04:47 +0000

    deploy: bac10875c174dafe2e39507b6fb3e593403c0d3f

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * Licensed under MPL 2.0 or greater. See URL for more information.
  */
 
-const version="102113200"
+const version="106399557"
 const cache_name = `stardew-valley-item-finder-v${version}`
 const cache = caches.open(cache_name)
 const requests = [
