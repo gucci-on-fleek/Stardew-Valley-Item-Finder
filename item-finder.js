4126e8bd56adfe260a6bce67bd9e355492db39cf
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2020-06-01 16:08:42 -0600

    Add Stardew Valley Wiki support Allow the Stardew Valley Wiki to be querried by clicking on an item name in the table.

--- item-finder.js
+++ item-finder.js
@@ -20,6 +20,9 @@ function create_template() {
         silver: document.getElementById('silver_base'),
         gold: document.getElementById('gold_base'),
         iridium: document.getElementById('iridium_base'),
+        wiki_query: document.getElementById('wiki_query_base'),
+        wiki_link: document.getElementById('wiki_link'),
+        wiki_search: document.getElementById('wiki_search'),
     }
 
     /**
@@ -43,6 +46,17 @@ function create_template() {
         silver: () => __template.silver.content.cloneNode(true),
         gold: () => __template.gold.content.cloneNode(true),
         iridium: () => __template.iridium.content.cloneNode(true),
+        wiki_query: () => __template.wiki_query.content.cloneNode(true),
+        wiki_link: function (x) {
+            const clone = __template.wiki_link.content.cloneNode(true)
+            clone.firstElementChild.href += encodeURIComponent(x)
+            return clone
+        },
+        wiki_search: function (x) {
+            const clone = __template.wiki_search.content.cloneNode(true)
+            clone.firstElementChild.href += encodeURIComponent(x)
+            return clone
+        },
     }
 }
 
@@ -112,6 +126,7 @@ function csv_to_table(csv) {
     table = make_header(csv_array, table);
     set_output(table);
     enable_table_sort();
+    enable_wiki_click()
 }
 
 
@@ -456,6 +471,7 @@ function filter_table() {
     const filter_class_name = `filter_${filter_class}`;
     const last_last_filter_class_name = `filter_${last_last_filter_class}`;
 
+    remove_descriptions()
     for (let i = 0; i < rows.length; i++) {
         let row = rows[i];
         if (!row.textContent.match(search)) {
@@ -473,4 +489,142 @@ function filter_table() {
     const new_foot = calculate_sum(table) // Update the footer after the filter is applied
 
     filter_class++; // Increment the class's name
+}
+
+
+/**
+ * Make a request to the *Stardew Valley Wiki*.
+ * @param {Function} callback - The function to be called with the resultant data 
+ * @param {Map<String, String>} parameters - The URL parameters to be used for the request
+ * @param {Map<String, String>} [default_parameters] - The default parameters URL parameters to use. Only use if you need to override the default parameters.
+ * @remarks The Wiki is using a *really* old version of MediaWiki
+ *          (circa 2016), so its CORS support is broken. Although
+ *          there appears to be some CORS support with the `origin`
+ *          parameter, the server will not return an `Access-
+ *          Control-Allow-Origin` header, so it is not possible to
+ *          make a request this way. Because of this, we need to
+ *          fallback to making a JSONP request. JSONP isn't a very
+ *          good method for making requests, but the only alternative
+ *          is to proxy the requests with a properly-configured
+ *          server.
+ * @effects Inserts a `script` element into the DOM which ultimately makes a network request.
+ *          
+ */
+function make_wiki_request(callback, parameters, default_parameters = {
+    action: 'query',
+    format: 'json',
+    formatversion: '2',
+    redirects: ''
+}) {
+    parameters = new URLSearchParams({ ...parameters, ...default_parameters, callback: callback.name })
+
+    const request_element = template.wiki_query()
+    request_element.firstElementChild.src += parameters.toString()
+    document.head.appendChild(request_element)
+}
+
+
+let _wiki_callback_success, _wiki_callback_failure // Globals, holds resolve/reject functions so that promises can be used
+/**
+ * Get the description of an item from the *Stardew Valley Wiki* using its name.
+ * @param {String} title - The title of the page to request
+ * @returns {Promise<String>} A promise representing the network request.
+ * @remarks Because we need to make a JSONP request, we can't use promises
+ *          without a wrapper. This function is just a wrapper that allows
+ *          us to use promises. Sadly, we need to use 2 global variables
+ *          to accomplish this.
+ * @effects Modifies the global variables `_wiki_callback_*`. Calls
+ *          `make_wiki_request` which ultimately makes a network
+ *          request. Also modifies the DOM to remove the JSONP 
+ *          `script` element.
+ */
+function wiki_description_by_title(title) {
+    make_wiki_request(_wiki_callback, { titles: title, prop: 'pageprops' })
+    const request = new Promise((accept, reject) => { _wiki_callback_success = accept; _wiki_callback_failure = reject })
+    return request.finally(() => document.getElementById('wiki_query').remove()) // Cleanup
+}
+
+
+/**
+ * Private. The callback used by the JSONP request.
+ * @internal
+ * @param {Object} data - The data returned by the `Stardew Valley Wiki`.
+ * @effects Calls the `_wiki_callback_*` to resolve a promise.
+ */
+function _wiki_callback(data) {
+    try {
+        const result = data.query.pages[0].pageprops.description
+        _wiki_callback_success(result)
+    } catch {
+        _wiki_callback_failure("Item not found.")
+    }
+}
+
+
+/**
+ * Allows the user to click on an item name and view its description from the *Stardew Valley Wiki*
+ * @effects Adds event listeners. Modifies the DOM. Calls functions which make network requests. 
+ */
+function enable_wiki_click() {
+    const table = document.getElementById('item_table')
+    const body = table.tBodies[0]
+
+    body.addEventListener('click', function (event) {
+        const target = event.target
+        if (target.cellIndex !== 0) { // Only capture click events on the first row
+            return
+        }
+        const page_title = normalize_names(target.textContent)
+
+        let cell // Allow the cell to be carried through each finally/then/catch stages
+        wiki_description_by_title(page_title).finally(function () {
+            remove_descriptions()
+            const row = body.insertRow(target.parentElement.rowIndex)
+            row.className = 'item_description'
+            cell = row.insertCell()
+            cell.colSpan = 6
+
+        }).then(function (result) {
+            cell.innerHTML = result // use innerHTML because the result can contain character entities
+            cell.appendChild(template.wiki_link(page_title))
+
+        }).catch(function (result) {
+            cell.innerHTML = result
+            cell.appendChild(template.wiki_search(page_title))
+        })
+    })
+    table.addEventListener('beforeSort', remove_descriptions) // The colspan attributes cause problems with sorting
+}
+
+
+/**
+ * Removes old Wiki description rows.
+ * @effects Modifies DOM.
+ */
+function remove_descriptions() {
+    const descriptions = document.querySelectorAll('.item_description')
+    for (let i = 0; i < descriptions.length; i++) {
+        descriptions[i].remove()
+    }
+}
+
+
+const _prefixed_items = ['Honey', 'Juice', 'Wine', 'Jelly', 'Aged Roe', 'Roe'] // Constant that lists all items that need to be normalized before a Wiki request.
+/**
+ * Normalize item names for the *Stardew Valley Wiki* request.
+ * @param {String} name - The name of the item
+ * @returns {String} The item name after normalization.
+ * @remarks "Large Goat Milk" is listed as "L. ..." in the save file
+ *          but there is no redirect for this in the Wiki. The other
+ *          items that need to be normalized are all nouns, except for
+ *          "pickled", which is corrected to its noun form.
+ * @effects None
+ */
+function normalize_names(name) {
+    if (name.includes('L. Goat Milk')) { return 'Large Goat Milk' }
+    if (name.includes('Pickled')) { return 'Pickles' }
+    for (let i = 0; i < _prefixed_items.length; i++) {
+        if (name.includes(_prefixed_items[i])) { return _prefixed_items[i] }
+    }
+    return name
 }
