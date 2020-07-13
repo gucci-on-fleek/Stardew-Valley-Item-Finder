acce1bb0ef31a86548af1a7f99167c7bb889bce2
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2020-07-13 15:59:11 -0600

    Update build.sh Minify images and add support for builds in Docker

--- build.sh
+++ build.sh
@@ -12,7 +12,11 @@ set -e # Fail the entire build if any command fails
 
 install_dependencies () { 
     sudo npm i -g csso-cli terser html-minifier # csso to minify CSS, terser to minify JS, and html-minifier to minify HTML
-    sudo apt install minify # to minify the XSLT
+    sudo apt install minify pngcrush zopfli --no-install-recommends -y # to minify XSLT and PNG
+
+    wget "https://github.com/RazrFalcon/svgcleaner/releases/download/v0.9.5/svgcleaner_linux_x86_64_0.9.5.tar.gz" # to minify SVG
+    tar xf svgcleaner* -C ~
+
 }
 
 unique_cache_name () {
@@ -34,13 +38,30 @@ minify_css () {
 }
 
 minify_xslt () {
-    minify --type=xml -o items.min.xslt items.xslt
-    minify --type=xml -o items-to-csv.min.xslt items-to-csv.xslt
-    minify --type=xml -o price-adjustments.min.xslt price-adjustments.xslt
+    command minify --type=xml -o items.min.xslt items.xslt
+    command minify --type=xml -o items-to-csv.min.xslt items-to-csv.xslt
+    command minify --type=xml -o price-adjustments.min.xslt price-adjustments.xslt
 }
 
 minify_html () {
     html-minifier index.html --collapse-boolean-attributes --collapse-inline-tag-whitespace --collapse-whitespace --conservative-collapse --decode-entities --html5 --include-auto-generated-tags --remove-attribute-quotes --remove-comments --remove-empty-attributes --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes -o index.html
+}
+
+minify_one_png () {
+    pngcrush -ow -rem gAMA -rem cHRM -rem bKGD -rem tIME -rem tEXt -rem TRRd "$1" # Remove extra data in png files
+    zopflipng -y -m --lossy_transparent "$1" "$1" # Bruteforce compress the png
+}
+
+minify_one_svg () {
+    for i in $(seq 5); do # For some reason, svgcleaner needs to be ran multiple times for the best results
+        ~/svgcleaner "$1" "$1" --remove-invisible-elements
+    done
+}
+
+minify_images () {
+    # Recursively minify all images
+    find . -name "*.png" -exec "$0" minify_one_png {} \;
+    find . -name "*.svg" -exec "$0" minify_one_svg {} \;
 }
 
 minify () {
@@ -49,6 +70,7 @@ minify () {
     minify_css
     minify_xslt
     minify_html
+    minify_images
 }
 
 github_build () {
@@ -57,6 +79,14 @@ github_build () {
     minify
 }
 
+docker_build () {
+    apt update
+    apt install sudo
+    apt install npm wget --no-install-recommends -y
+    install_dependencies
+    minify
+}
+
 
 echo "$1"
-$1
+$1 "$2"
