bd5dc31fda9898fbcb6c2e4fb4260f70bc281649
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2021-05-10 23:52:49 -0600

    Switch to JS modules
    This makes imports a little easier and prevents global namespace
    pollution.

--- item-finder.js
+++ item-finder.js
@@ -1,6 +1,5 @@
 // @ts-check
 
-"use strict"
 /*
  * Stardew Valley Item Finder
  * https://gucci-on-fleek.github.io/Stardew-Valley-Item-Finder/
@@ -8,7 +7,7 @@
  * SPDX-FileCopyrightText: 2021 gucci-on-fleek
  */
 
-/* global Tablesort */
+import {Tablesort} from "./libraries/tablesort.min.js" // eslint-disable-line id-match
 
 /**
  * @type {Object<String, Function>}
@@ -553,11 +552,8 @@ function calculate_sum(table) {
  */
 function enable_table_sort() {
     const item_table = elements.item_table
-
-    // @ts-expect-error I can assure you that `Tablesort` is defined
     const tablesort = new Tablesort(item_table) // Allow the table headings to be used for sorting
 
-    // @ts-expect-error
     Tablesort.extend("number",
         item => item.match(/\d/), // Sort numerically
         (a, b) => parse_integer(a) - parse_integer(b))
