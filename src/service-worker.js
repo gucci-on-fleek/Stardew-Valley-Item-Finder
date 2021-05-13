4847f5df0d30709f0ce5906c2bf1e90513281903
gucci-on-fleek <gucci-on-fleek@users.noreply.github.com>
2021-05-13 22:15:37 +0000

    deploy: 771b292d9c619eb951478a01a84ceb738bf4e3b9

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,7 +5,7 @@
  * SPDX-FileCopyrightText: 2021 gucci-on-fleek
  */
 
-const version="840345830"
+const version="840360923"
 const cache_name = `stardew-valley-item-finder-v${version}`
 const cache = caches.open(cache_name)
 const requests = [
